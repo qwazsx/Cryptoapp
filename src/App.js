da878dc2ed9a09c031df9a5d84f5dfb96413c8a7
FATIH CAMBEK <fatih_cambek@hotmail.com>
2022-02-13 17:57:47 +0300

    Initialize Cryptocurrency App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+// rafce diye bir snippet kullanarak iskeletimizi oluşturduk.
+// Kullanılan paketler
+// npm install antd @ant-design/icons react-redux @reduxjs/toolkit axios chart.js html-react-parser millify moment react-chartjs-2 react-router-dom
+import React from 'react'
+import { Switch, Route, Link } from 'react-router-dom';
+import { Layout, Typography, Space } from 'antd';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// Bütün componentler için tek tek import yazmak yerine components altına index.js ekleyerek onun içine yazdık
+// Tek bir import satırı ile bütün componentlerimizi kulllanabilir hale geldik
+import { Navbar } from './components'
+import './App.css'
+
+
+const App = () => {
+    return (
+        <div className="app">
+            <div className="navbar">
+                <Navbar/>
+            </div>
+            <div className="main">
+
+            </div>
+            <div className="footer">
+
+            </div>
+        </div>
+    )
 }
 
-export default App;
+export default App
