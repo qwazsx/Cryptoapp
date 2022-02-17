597f822f9100581f3a4dd340ac2cd48dba98783d
FATIH CAMBEK <fatih_cambek@hotmail.com>
2022-02-17 20:00:23 +0300

    İskelet komponentleri ekledik

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-// rafce diye bir snippet kullanarak iskeletimizi oluşturduk.
+// rafce diye bir snippet kullanarak iskeletimizi oluşturduk. jsx componentlerimizde
 // Kullanılan paketler
 // npm install antd @ant-design/icons react-redux @reduxjs/toolkit axios chart.js html-react-parser millify moment react-chartjs-2 react-router-dom
 import React from 'react'
-import { Switch, Route, Link } from 'react-router-dom';
+import { Routes, Switch, Route, Link } from 'react-router-dom';
 import { Layout, Typography, Space } from 'antd';
 
 // Bütün componentler için tek tek import yazmak yerine components altına index.js ekleyerek onun içine yazdık
 // Tek bir import satırı ile bütün componentlerimizi kulllanabilir hale geldik
-import { Navbar } from './components'
+import { Navbar, Homepage, Exchanges, Cryptocurrencies, CryptoDetails, News } from './components'
 import './App.css'
 
 
@@ -15,13 +15,45 @@ const App = () => {
     return (
         <div className="app">
             <div className="navbar">
-                <Navbar/>
+                <Navbar />
             </div>
             <div className="main">
-
-            </div>
-            <div className="footer">
-
+                <Layout>
+                    <div className="routes">
+                        <Routes>
+                            <Route exact path='/' element={<Homepage />}>
+                            </Route>
+                            <Route exact path='/exchanges' element={<Exchanges />}>
+                            </Route>
+                            <Route exact path='/cryptocurrencies' element={<Cryptocurrencies />}>
+                            </Route>
+                            <Route exact path='/crypto/:coinId' element={<CryptoDetails />}> {/* buraya index geçicez 1-2 gibi */}
+                            </Route>
+                            <Route exact path='/news' element={<News />}>
+                            </Route>
+                            {/* 
+                            <Switch>
+                                <Route exact path='/news'>
+                                    <News />
+                                </Route>
+                            </Switch> 
+                            react-router-dom 6.0. öncesi yukarıdaki gibi kullanılıyordu. Yeni versiyonda element diye belirtmek gerekiyor.
+                            Switch tagi de Routes tagine dönüştü.
+                            */}
+                        </Routes>
+                    </div>
+                </Layout>
+                <div className="footer">
+                    <Typography.Title level={5} style={{ color: 'white', textAlign: 'center' }}>
+                        Cryptoverse <br />
+                        All rights reserved.
+                    </Typography.Title>
+                    <Space>
+                        <Link to='/'>Home</Link>
+                        <Link to='/exchanges'>Exchanges</Link>
+                        <Link to='/news'>News</Link>
+                    </Space>
+                </div>
             </div>
         </div>
     )
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Button, Menu, Typography, Avatar } from 'antd'
-import{Link} from 'react-router-dom'
-import { HomeOutlined,MoneyCollectOutlined, BulbOutlined, FundOutlined, MenuOutlined } from '@ant-design/icons'
+import { Link } from 'react-router-dom'
+import { HomeOutlined, MoneyCollectOutlined, BulbOutlined, FundOutlined, MenuOutlined } from '@ant-design/icons'
 
 import icon from '../images/cryptocurrency.png'
 
@@ -17,6 +17,20 @@ const Navbar = () => {
 
                 </Button> */}
             </div>
+            <Menu theme='dark'>
+                <Menu.Item icon={<HomeOutlined />}>
+                    <Link to="/">Home</Link>
+                </Menu.Item>
+                <Menu.Item icon={<FundOutlined />}>
+                    <Link to="/cryptocurrencies">Cryptocurrencies</Link>
+                </Menu.Item>
+                <Menu.Item icon={<MoneyCollectOutlined />}>
+                    <Link to="/exchanges">Exchanges</Link>
+                </Menu.Item>
+                <Menu.Item icon={<BulbOutlined />}>
+                    <Link to="/news">News</Link>
+                </Menu.Item>
+            </Menu>
         </div>
     )
 }
